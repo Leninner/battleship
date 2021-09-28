129d317b796779a4c9f5e36d9460e516d46cb403
leninner <mazabandalenin180@gmail.com>
2021-09-28 17:48:55 -0500

    Añado función de fábrica para crear naves y métodos para golpear

--- src/shipFactory.js
+++ src/shipFactory.js
@@ -2,4 +2,27 @@
  * longitud, dónde han sido golpeados y si se han hundido o no.
  */
 
-class Ship {}
+const Ship = (length) => {
+  const getLength = () => length;
+
+  const hit = () => 1;
+
+  const sunk = (damage) => {
+    length -= damage;
+    return length - damage <= 0 ? true : false;
+  };
+
+  return { getLength, hit, sunk };
+};
+
+const star = Ship(4);
+const lenin = Ship(2);
+
+console.log(star.sunk(lenin.hit()));
+console.log(star.getLength());
+console.log(star.sunk(lenin.hit()));
+console.log(star.getLength());
+console.log(star.sunk(lenin.hit()));
+console.log(star.getLength());
+console.log(star.sunk(lenin.hit()));
+console.log(star.getLength());

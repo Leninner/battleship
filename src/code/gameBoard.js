04913df897c28fe6aad92cc162d99d93c24fc3a4
leninner <mazabandalenin180@gmail.com>
2021-09-29 15:32:03 -0500

    Update

--- src/code/gameBoard.js
+++ src/code/gameBoard.js
@@ -2,39 +2,34 @@ import { Ship } from './shipFactory.js';
 
 //Debo enviar coordenadas al momento de desplegar cada figura en el tablero de juego
 
+const doBoard = (elemento, board) => {
+  //Añado evento de escucha a cada cajita a través de delegación de eventos
+  elemento.addEventListener('click', (e) => {
+    if (e.target.classList[0] === 'prueba') {
+      e.target.classList.add('active');
+    }
+  });
+
+  board.forEach((element) => {
+    element.forEach((element) => {
+      let doit = document.createElement('div');
+      doit.classList.add('prueba');
+      doit.setAttribute('id', element);
+      elemento.append(doit);
+    });
+  });
+};
+
 const Gameboard = (name) => {
   const getName = () => name;
+
   const displayBoard = (board, name) => {
     if (name === 'user') {
-      const user = document.querySelector('.user');
-      user.addEventListener('click', (e) => {
-        if (e.target.classList[0] === 'prueba') {
-          e.target.classList.add('active');
-        }
-      });
-
-      board.forEach((element) => {
-        element.forEach((element) => {
-          let doit = document.createElement('div');
-          doit.classList.add('prueba');
-          user.append(doit);
-        });
-      });
+      const user = document.querySelector(`.user`);
+      doBoard(user, board);
     } else {
       const com = document.querySelector('.com');
-      com.addEventListener('click', (e) => {
-        if (e.target.classList[0] === 'prueba') {
-          e.target.classList.add('active');
-        }
-      });
-
-      board.forEach((element) => {
-        element.forEach((element) => {
-          let doit = document.createElement('div');
-          doit.classList.add('prueba');
-          com.append(doit);
-        });
-      });
+      doBoard(com, board);
     }
   };
 

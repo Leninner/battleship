dafec9d7e8d536307d8f9592bca2e1b5c7ded153
leninner <mazabandalenin180@gmail.com>
2021-09-29 09:53:40 -0500

    Añado redo

--- src/shipFactory.js
+++ src/shipFactory.js
@@ -2,22 +2,30 @@
  * longitud, dónde han sido golpeados y si se han hundido o no.
  */
 
-const Ship = (length) => {
-  const getLength = () => length;
+const Ship = (arr) => {
+  const getSize = () => arr;
 
-  const hit = (damage) => {
-    length -= damage;
-    if (length === 0) {
-      return sunk();
-    }
-    return length;
+  const hit = (position) => {
+    arr[position] = 0;
   };
 
-  const sunk = () => {
-    return true;
+  const isSunk = () => {
+    return arr.every((element) => element === 0) ? true : false;
   };
 
-  return { getLength, hit, sunk };
+  return { getSize, hit, isSunk };
 };
 
-export { Ship };
+const len = Ship([1, 1, 1, 1]);
+
+len.hit(2);
+console.log(len.isSunk());
+len.hit(1);
+len.hit(0);
+len.hit(3);
+console.log(len.isSunk());
+console.log(len.getSize());
+
+// En cada golpe, debemos comprobar si está hundido o sigue vivo
+
+// export { Ship };
